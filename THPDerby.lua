d7a0eb1543f0f1f2dc0f67b33ee75885d1954684
thehoneypotgg <www.com.com.email@gmail.com>
2025-09-12 22:54:03 -0700

    THP DD Preset v1

---
+++ THPDerby.lua
@@ -0,0 +1,16 @@
+-- THP Derby
+local THPDerby = {
+	name = "THPDerby",
+	realname = "THPDerby",
+	pregametime = 30,
+	scaledtimetoggle = "Off",
+	detonationtime = 15,
+	ringtoggle = "On",
+	scaledringtoggle = "off",
+	jackpotvalue = 40,
+	overtimetoggle = "on",
+	lastpassedlimit = 2,
+	overtimelimit = 5,
+}
+
+BDD_AddPreset(THPDerby)
